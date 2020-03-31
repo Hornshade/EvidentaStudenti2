4ef1f9d55ae1970c8507b9bb9f13ee778f826232
Hornshade <sabin.mihai@yahoo.com>
2020-03-31 15:31:48 +0300

    First commit

---
+++ LibrarieEntitati/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[][] note=new int[4][];
+
+        // contructor implicit
+        public Student()
+        {
+            for (int i = 0; i < 4; i++)
+                note[i] = new int[15];
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+            for (int i = 0; i < 4; i++)
+                note[i] = new int[15];
+        }
+
+        public void SetNote(string sirNote)
+        {
+            
+            // initializare vector cu note
+            string[] note_s = sirNote.Split(',');
+            // DE IMPLEMENTAT:
+            // extrage note din sir si adauga in vectorul note
+    
+            string[] splitNota;
+            int contor_an = 0;
+            
+            foreach(var nota in note_s)
+            {
+                int contor_coloana = 0;
+                splitNota = nota.Split(' ');
+                foreach(var nota_an in splitNota)
+                {
+                    note[contor_an][contor_coloana] = Convert.ToInt32(nota_an);
+
+                    contor_coloana++;
+                }
+                contor_an++;  
+                
+            }
+            
+
+        }
+        public void ConversieLaSir()
+        {
+            string afisare;
+            Console.WriteLine("Studentul {0} are notele : ...", nume);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Notele din anul {0}", i + 1);
+                afisare = string.Join(",", note[i]);
+                Console.WriteLine(afisare);
+
+            }
+        }
+
+
+    }
+}
